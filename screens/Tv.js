e28cef5c0a41ceabcdc9641b6318c7b89591e7fc
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-18 00:50:00 +0900

    set: Tab.screen style setting

--- screens/Tv.js
+++ screens/Tv.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 const Tv = () => {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Tv</Text>
     </View>
   );
