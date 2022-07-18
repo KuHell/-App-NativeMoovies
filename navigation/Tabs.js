c51f6037057f8905f1c9bbf35341ea2d50a037cf
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-19 00:42:41 +0900

    Tap.screenOption[tabBarLabelStyle]

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -8,7 +8,10 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator initialRouteName="Tv">
+    <Tab.Navigator
+      initialRouteName="Tv"
+      screenOptions={{ tabBarLabelStyle: { color: "red" } }}
+    >
       <Tab.Screen name="Movie" component={Movie} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
