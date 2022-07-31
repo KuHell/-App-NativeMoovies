d97a71f5db81da9a8d409216b879e18b67fec65b
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-31 15:02:03 +0900

    ad

--- screens/Search.js
+++ screens/Search.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const Search = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Search</Text>
+      <Text>Search: kingsman</Text>
     </View>
   );
 };
