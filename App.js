b1d6f44feb095400dba8c235cbd35e1bce1711f8
KuHell <itkdgus489@gmail.com>
2022-09-16 15:11:20 +0900

    axios 테스트

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stack from "./navigation/Stack";
+import axios from "axios";
 
 export default function App() {
+  // useEffect(() => {
+  //   const body = {
+  //     email: "1234",
+  //   };
+  //   axios
+  //     .post("http://15.165.158.22:7272/api/game/rnTest", body)
+  //     .then((result) => {
+  //       console.log("result: ", result);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
   return (
     <NavigationContainer>
       <Stack />
