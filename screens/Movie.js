f1324f19c625c27ef0fcba17e94c2c2e000d4670
KuHell <itkdgus489@gmail.com>
2022-09-19 12:17:40 +0900

    styled-components install

--- screens/Movie.js
+++ screens/Movie.js
@@ -1,14 +1,25 @@
 import React from "react";
+import styled from "styled-components/native";
 import { View, Text, TouchableOpacity } from "react-native";
+
+const Btn = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: blue;
+`;
 
 const Movie = ({ navigation: { navigate } }) => {
   return (
-    <TouchableOpacity
+    <Btn
       onPress={() => navigate("Stack", { screen: "Three" })}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      // style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
       <Text>Movie</Text>
-    </TouchableOpacity>
+    </Btn>
   );
 };
 
