908d4292d0417e4f3d5c5be94f217a580faee6e5
KuHell <itkdgus489@gmail.com>
2022-09-19 17:24:04 +0900

    add: pacage

---
+++ screens/Movie.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styled from "styled-components/native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+
+const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({
+
+}) => null;
+
+export default Movie;
